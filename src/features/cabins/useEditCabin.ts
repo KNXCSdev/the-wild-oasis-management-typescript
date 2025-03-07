81db8943bd04fdd53f0b4fc043c62122b5fe88aa
KNXCSdev <michal.miksho@gmail.com>
2025-03-07 15:39:45 +0100

    Updating Settings,custom hooks

---
+++ src/features/cabins/useEditCabin.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { CabinsEdit } from "../../utils/types";
+
+export function useEditCabin() {
+  const queryClient = useQueryClient();
+  const { mutate: editCabin, isPending: isEditing } = useMutation({
+    mutationFn: ({ newCabinData, id }: { newCabinData: CabinsEdit; id: number }) =>
+      createEditCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success("Cabin successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+  });
+
+  return { editCabin, isEditing };
+}
