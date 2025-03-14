2e08693f8d11c5a8d0505bfdb92e011f31c51d55
KNXCSdev <michal.miksho@gmail.com>
2025-03-14 20:44:44 +0100

    DarkMode

--- src/ui/DarkModeToggle.tsx
+++ src/ui/DarkModeToggle.tsx
@@ -1,16 +1,15 @@
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
-// import { useDarkMode } from "../context/DarkModeContext";
+import { useDarkMode } from "../context/DarkModeContext";
 
 function DarkModeToggle() {
-  return "";
-  // const { isDarkMode, toggleDarkMode } = useDarkMode();
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
 
-  // return (
-  //   <ButtonIcon onClick={toggleDarkMode}>
-  //     {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
-  //   </ButtonIcon>
-  // );
+  return (
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </ButtonIcon>
+  );
 }
 
 export default DarkModeToggle;
